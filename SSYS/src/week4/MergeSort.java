57a9cc20ee6e6bcdea2adea3ed8bcac6cbe0f02b
Jordy van der Zwan <jordyvdzwan@hotmail.com>
2015-12-08 13:26:05 +0100

    fixed mergesort

--- SSYS/src/week4/MergeSort.java
+++ SSYS/src/week4/MergeSort.java
@@ -5,47 +5,32 @@ import java.util.*;
 public class MergeSort {
     public static <E extends Comparable<E>>
     
-    void mergesort(List<E> list) {
-    	if (list.size() > 1) {
+    List<E> mergesort(List<E> list) {
+     	List<E> res = new ArrayList();
+    	if (list.size() <= 1) {
+    		res = list;
+    	} else {	
 	    	int i = list.size() / 2;
-	    	List<E> fst;
-	    	List<E> snd;
-	    	if (i >= 2) {
-	    		fst = list.subList(0, i - 1);
-	        	snd = list.subList(i, list.size());
-	        	mergesort(fst);
-	        	mergesort(snd);
-	    	} else {
-	    		fst = list.subList(0, 0);
-	        	snd = list.subList(1, 1);
-	    	}
-	    	
+	    	List<E> fst = mergesort(list.subList(0, i));
+	    	List<E> snd = mergesort(list.subList(i, list.size()));
 	    	int fi = 0;
 	    	int si = 0;
-	    	List<E> res = new ArrayList();
-	    	
-	    	if (list.size() > 1) {
-				while (fi < fst.size() && si < snd.size()) {
-					if (fst.get(fi).compareTo(snd.get(si)) < 0) {
-						res.add(fst.get(fi));	
-						fi = fi + 1;
-					} else {
-						res.add(snd.get(si));
-						si = si + 1;
-					}
-	//				if (fi < fst.size()) {
-	//					res.add(fst.get(fi));
-	//				} 
-	//				if (si < snd.size()) {
-	//					res.add(snd.get(si));
-	//				
-	//				}
+	    	while (fi < fst.size() && si < snd.size()) {
+	    		if (fst.get(fi).compareTo(snd.get(si)) < 0) {
+					res.add(fst.get(fi));	
+					fi = fi + 1;
+				} else {
+					res.add(snd.get(si));
+					si = si + 1;
 				}
+			} 	
+	    	if (fi < fst.size()) {
+				res.addAll(fst.subList(fi, fst.size()));
+			} 
+			if (si < snd.size()) {
+				res.addAll(snd.subList(si, snd.size()));
 			}
-	    	list.clear();
-	    	for (int j = 0; j < res.size(); j++) {
-	    		list.(res.get(j));
-	    	}
     	}
+    	return res;
     }
 }
--- SSYS/src/week4/MergeSortTest.java
+++ SSYS/src/week4/MergeSortTest.java
@@ -21,29 +21,29 @@ public class MergeSortTest {
     @Test
     public void testMergesortSingleItem() {
         List<Integer> sequence = new ArrayList<>(Arrays.asList(1));
-        MergeSort.mergesort(sequence);
+        sequence = MergeSort.mergesort(sequence);
         assertEquals(sequence, Arrays.asList(1));
     }
 
     @Test
     public void testMergesortSortedList() {
         List<Integer> sequence = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-        MergeSort.mergesort(sequence);
+        sequence = MergeSort.mergesort(sequence);
         assertEquals(sequence, Arrays.asList(1, 2, 3, 4, 5));
 
         sequence = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        MergeSort.mergesort(sequence);
+        sequence = MergeSort.mergesort(sequence);
         assertEquals(sequence, Arrays.asList(1, 2, 3, 4, 5, 6));
     }
 
     @Test
     public void testMergesortUnsortedList() {
         List<Integer> sequence = new ArrayList<>(Arrays.asList(3, 2, 1, 5, 4));
-        MergeSort.mergesort(sequence);
-        assertEquals(MergeSort.mergesort(sequence), Arrays.asList(1, 2, 3, 4, 5));
+        sequence = MergeSort.mergesort(sequence);
+        assertEquals(sequence, Arrays.asList(1, 2, 3, 4, 5));
 
         sequence = new ArrayList<>(Arrays.asList(3, 2, 1, 6, 5, 4));
-        MergeSort.mergesort(sequence);
+        sequence = MergeSort.mergesort(sequence);
         assertEquals(sequence, Arrays.asList(1, 2, 3, 4, 5, 6));
     }
 }
