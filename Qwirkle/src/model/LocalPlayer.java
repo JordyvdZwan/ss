d94fd180bbd972dd5d161dde1bf4c993c0dce6de
reinier <reinierstribos@gmail.com>
2016-01-12 21:17:32 +0100

    wie dit leest doet BIT

--- Qwirkle/src/model/LocalPlayer.java
+++ Qwirkle/src/model/LocalPlayer.java
@@ -1,5 +1,20 @@
 package model;
 
-public interface LocalPlayer {
+public class LocalPlayer implements Player {
+	public String name;
+	public int score;
+	
+	public LocalPlayer(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
 
 }
